ca2fc507e31c4f0d74d28a9dff4a5f07876e6eb1
Arindam <arismart99@gmail.com>
2012-07-03 00:28:51 +0530

    Initial generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aapaurmain::Application.config.secret_token = 'cfca367b2b53c10609ccbf5a6adff1401ca2e7003a658a16c4f6e27c986b0a576f44eec80fdd76147e9b4b9e78247a8fc910e39690cf965abbfe0b5f625d367b'

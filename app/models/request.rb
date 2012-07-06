993fc350a3f69054a4e4d1364c3ef316f3802ea1
Arindam <arismart99@gmail.com>
2012-07-07 00:09:42 +0530

    Request basic operations on Models

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,63 @@
+##############################################################################
+# @attr [Fixnum(11)] id             
+# @attr [Fixnum(11)] from_id        
+# @attr [Fixnum(11)] to_id          
+# @attr [Fixnum(4)] status         
+# @attr [Date] approved_date  
+# @attr [Date] rejected_date  
+# @attr [Date] asked_date     
+# @attr [Date] withdraw_date  
+# @attr [Time] created_at     
+# @attr [Time] updated_at     
+##############################################################################
 class Request < ActiveRecord::Base
-  # attr_accessible :title, :body
+  
+  # Enumeration for status
+  ASKED = 0
+  ACCEPTED = 1
+  DECLINED = 2
+  WITHDRAWN = 3
+  
+  # Finds the relevant request object and sets status to accepted
+  # @param [Fixnum] to_id
+  #     The id of the user to which the request was sent, and who approved it
+  # @param [Fixnum] from_id
+  #     The id of the user who sent the request.
+  def self.set_as_accepted(to_id, from_id)
+    req = Request.find_by_from_id_and_to_id(from_id, to_id) rescue nil
+    if req
+      req.status = ACCEPTED
+      req.approved_date = Time.now.to_date
+      req.save!
+    end
+  end
+  
+  # Finds the relevant request object and sets status to declined
+  # @param [Fixnum] to_id
+  #     The id of the user to which the request was sent, and who declined it
+  # @param [Fixnum] from_id
+  #     The id of the user who sent the request.
+  def self.set_as_declined(to_id, from_id)
+    req = Request.find_by_from_id_and_to_id(from_id, to_id) rescue nil
+    if req
+      req.status = DECLINED
+      req.rejected_date = Time.now.to_date
+      req.save!
+    end
+  end
+  
+  # Finds the relevant request object and sets status to withdrawn
+  # @param [Fixnum] to_id
+  #     The id of the user to which the request was sent
+  # @param [Fixnum] from_id
+  #     The id of the user who sent the request, and who withdrew it
+  def self.set_as_withdrawn(to_id, from_id)
+    req = Request.find_by_from_id_and_to_id(from_id, to_id) rescue nil
+    if req
+      req.status = WITHDRAWN
+      req.withdraw_date = Time.now.to_date
+      req.save!
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,8 +55,7 @@ class User < ActiveRecord::Base
   
   # add types for profession
   
-  has_many :request
-  has_many :recommendation
+  has_many :recommendation, :dependent => destroy
   has_one :subscription
   
   ### PAID SECTION STARTS ###
@@ -100,4 +99,53 @@ class User < ActiveRecord::Base
   end
   
   ### LOCK SECTION ENDS ###
+  
+  ### REQUEST SECTION STARTS ###
+  
+  # Creates a new request object for a & b.
+  # Also sends notification to b.
+  # @param [User] b
+  #     The receiver of the request 
+  def create_new_request(b)
+    req = Request.create
+    req.from_id = self.id
+    req.to_id = b.id
+    req.asked_date = Time.now.to_date
+    req.save!
+    
+    Notifier.deliver_request_notification(b)
+  end
+  
+  # Marks the request between self and from_user as accepted
+  # Puts self and from_user in locked state
+  # Sends notification to sender of request
+  # @param [User] from_user
+  #     The user who had sent the request
+  def accept_request_from(from_user)
+    Request.set_as_accepted(self.id, from_user.id)
+    self.lock_users(self, from_user)
+    Notifier.deliver_accepted_notification(from_user)
+    remove_from_searches(self, from_user)
+  end
+  
+  # Marks the request between self and from_user as declined
+  # Sends notification to sender of request
+  # @param [User] from_user
+  #     The user who had sent the request
+  def decline_request_from(from_user)
+    Request.set_as_declined(self.id, from_user.id)
+    Notifier.deliver_declined_notification(from_user)
+  end
+  
+  # Marks the request between self and from_user as withdrawn
+  # Removes the notification from self to the recipient
+  # @param [User] to_user
+  #     The user to whom the request was sent
+  def withdraw_request_to(to_user)
+    Request.set_as_withdrawn(to_user.id, self.id)
+    Notifier.remove_withdrawn_notification(self, from_user)
+  end
+  
+  ### REQUEST SECTION ENDS ###
+
 end

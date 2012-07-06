95328255e35efa2765eee11cf913149c267a2c44
Arindam <arismart99@gmail.com>
2012-07-06 22:29:47 +0530

    Adding methods to users and subcscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,48 @@
+##############################################################################
+# @attr [int(11)] id                 
+# @attr [date] start_date         
+# @attr [date] end_date           
+# @attr [tinyint(4)] subs_type          
+# @attr [date] remind_date_start  
+# @attr [int(11)] user_id            
+# @attr [date] renew_date         
+# @attr [tinyint(4)] renew_type         
+# @attr [datetime] created_at         
+# @attr [datetime] updated_at         
+##############################################################################
+
 class Subscription < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :user
+  
+  # Enumeration for subscription type
+  MONTHS_3 = 1
+  MONTHS_6 = 2
+  MONTHS_12 = 4
+  
+  # Enumeration for subscription status
+  ACTIVE = 0
+  EXPIRED = 1
+  
+  # Enumeration for remind time
+  REMIND_TIME = 7
+
+  # Sets the status to expired.
+  def expire_now
+    self.status = EXPIRED
+    self.save!
+  end
+  
+  # Renews subscription with the specified type
+  # Accordingly sets renew details, end date and remind date
+  # @param [Integer] renew_type
+  #     Enumerated value for renewal type
+  def renew_now(renew_type)
+    self.status = ACTIVE
+    self.renew_type = self.subs_type = renew_type
+    self.renew_date = Time.now.to_date
+    self.end_date = self.renew_date + (90 * renew_type).days
+    self.remind_date = self.end_date - REMIND_TIME.days
+    self.save!
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,103 @@
+#################################################################################
+# This class represents users. 
+#
+# @attr [Fixnum(11)] id                      
+# @attr [String(50)] name                    
+# @attr [Date] dob                     
+# @attr [String] sex                     
+# @attr [Fixnum(6)] family_preference       
+# @attr [Float] height                  
+# @attr [Fixnum(6)] spouse_preference       
+# @attr [Fixnum(20)] spouse_salary           
+# @attr [String(500)] further_education_plans 
+# @attr [String(500)] spouse_further_education
+# @attr [String(500)] settle_else             
+# @attr [Fixnum(6)] sexual_preference       
+# @attr [String(500)] virginity_opinion       
+# @attr [String(500)] ideal_marriage          
+# @attr [Fixnum(20)] salary                  
+# @attr [String(500)] hobbies                 
+# @attr [Fixnum(6)] siblings                
+# @attr [Fixnum(6)] profession              
+# @attr [String(500)] dream_for_future        
+# @attr [String(500)] interested_in           
+# @attr [String(500)] not_interested_in       
+# @attr [String(255)] settled_in              
+# @attr [true, false] dont_search             
+# @attr [Date] hidden_since            
+# @attr [DateTime] created_at              
+# @attr [DateTime] updated_at
+#
+#################################################################################
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+  
+  # Enumeration for family preference
+  FAMPREF_NUCLEAR = 0
+  FAMPREF_JOINT = 1
+  FAMPREF_EXTENDED = 2
+  
+  # Enumeration for spouse preference
+  SPOPREF_WORKING = 0
+  SPOPREF_HOMEMAKER = 1
+  
+  # Enumeration for sexual preference
+  SEXPREF_STRAIGHT = 0
+  SEXPREF_HOMOE = 1
+  SEXPREF_BI = 2
+  
+  # Enumeration for sex
+  MALE = 0
+  FEMALE = 1
+  
+  # Enumeration for availability `status`
+  AVAILABLE = 0
+  LOCKED = 1
+  
+  # add types for profession
+  
+  has_many :request
+  has_many :recommendation
+  has_one :subscription
+  
+  ### PAID SECTION STARTS ###
+  
+  # Returns whether the user is paid or not
+  # @return [TrueClass|FalseClass] 
+  def is_paid?
+    !self.subscription.blank?
+  end
+  
+  # Returns whether subscription has expired or not
+  # @return [TrueClass|FlaseClass]
+  def is_still_paid?
+    self.subscription.status == Subscription::ACTIVE
+  end
+  
+  ### PAID SECTION ENDS ###
+  
+  ### LOCK SECTION STARTS ###
+  
+  # Returns whether the user is available or in a locked state
+  # @return [TrueClass|FalseClass]
+  def is_available?
+    self.status == AVAILABLE
+  end
+  
+  # Locks the two users and saves their states
+  # @param [User] a
+  #     First User to be locked
+  # @param [User] b
+  #     Second User to be locked
+  def self.lock_users(a, b)
+    a.status = LOCKED
+    b.status = LOCKED
+    a.locked_since = b.locked_since = Time.now.to_date
+    a.locked_with = b
+    b.locked_with = a
+    
+    a.save!
+    b.save!
+  end
+  
+  ### LOCK SECTION ENDS ###
 end

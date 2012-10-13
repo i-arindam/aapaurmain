4d4d4131bc39420ffa0086663bf3656fb8651bb4
Arindam Chakraborty <arismart99@gmail.com>
2012-10-13 15:59:28 +0530

    Removing profile.css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -182,32 +182,6 @@ class UsersController < ApplicationController
     }
   end
   
-  # Fired when someone in the lock indicates a rejection
-  # The other person is sent a notification for approval
-  # @param [Fixnum(11)] id
-  # => The id of the person who requested the reject
-  # @return [JsonObject] denoting the 
-  # => success of the request, and
-  # => the corresponding message
-  def request_reject_locked
-    rejecting_user = User.find_by_id(params[:id])
-    render_404 and return unless rejecting_user and rejecting_user.status == User::LOCKED and rejecting_user == current_user
-    
-    to_approve_user = User.find_by_id(rejecting_user.locked_with)
-    render_404 and return unless to_approve_user and to_approve_user.status == User::LOCKED and to_approve_user.locked_with = params[:id]
-    
-    success = rejecting_user.request_mark_as_rejected
-    message = (success ? User::REQUEST_REJECT_SENT : User::REQUEST_REJECT_FAILED_TO_SAVE)
-    message.gsub!('{{user}}', to_approve_user.name)
-    
-    to_approve_user.send_request_for_confirming_reject if success
-    
-    render :json => {
-      :success => success,
-      :message => message
-    }    
-  end
-  
   ### POST LOCK STATE STARTS ###
   
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,6 @@ Aapaurmain::Application.routes.draw do
   post 'users/:id/locks/:lock_id/finalize' => 'users#finalize_lock'
   post 'users/:id/lock/request_confirm' => 'users#request_confirm_locked'
   post 'users/:id/lock/confirm_success' => 'users#confirm_success'
-  post 'users/:id/lock/request_reject' => 'users#request_reject_locked'
   
 
   # Search actions

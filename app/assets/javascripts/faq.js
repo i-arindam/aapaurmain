64a4d5eeb63b9fc2ca424f6c782d6ddfff86603f
Arindam Chakraborty <arismart99@gmail.com>
2012-10-19 16:27:50 +0530

    BUG FIX: faq, contact, layout

---
+++ app/assets/javascripts/faq.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  $('#faq-wrapper li').click(function(e) {
+    if($(this).hasClass('active')) {
+      return false;
+    }
+    e.preventDefault();
+    $('#faq-wrapper li.active').removeClass('active');
+    $(this).addClass('active');
+
+    var index = $('#faq-wrapper li').index(this);
+    $('.demo').not('.hide').addClass('hide');
+    $('.demo:eq(' + index + ')').removeClass('hide');
+    $('.demo:eq(' + index + ')').toggler({ initShow: "div.collapse:eq(0)"});
+    
+    var selector = '.demo:eq(' + index + ')';
+    $(selector).expandAll({
+      trigger: "div.expand", 
+      ref: "div.expand", 
+      showMethod: "slideDown", 
+      hideMethod: "slideUp", 
+      oneSwitch : false
+    });
+    
+  });
+  
+  $("#demo2 div.expand").toggler({initShow: "div.collapse:eq(0)"});
+ 
+  $("#demo2").expandAll({
+    trigger: "div.expand", 
+    ref: "div.expand", 
+    showMethod: "slideDown", 
+    hideMethod: "slideUp", 
+    oneSwitch : false
+  });
+});

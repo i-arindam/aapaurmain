b40d6b086be950209734bb6b018dd60ed3074ba8
apoorvi <apoorvi.kapoor@gmail.com>
2012-10-21 19:12:24 +0530

    FAQs Fixed

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -10,26 +10,26 @@ $(document).ready(function() {
     var index = $('#faq-wrapper li').index(this);
     $('.demo').not('.hide').addClass('hide');
     $('.demo:eq(' + index + ')').removeClass('hide');
-    $('.demo:eq(' + index + ')').toggler({ initShow: "div.collapse:eq(0)"});
+   // $('.demo:eq(' + index + ')').toggler({ initShow: "div.collapse:eq(0)"});
     
-    var selector = '.demo:eq(' + index + ')';
-    $(selector).expandAll({
-      trigger: "div.expand", 
-      ref: "div.expand", 
-      showMethod: "slideDown", 
-      hideMethod: "slideUp", 
-      oneSwitch : false
-    });
+    // var selector = '.demo:eq(' + index + ')';
+    // $(selector).expandAll({
+    //   trigger: "div.expand", 
+    //   ref: "div.expand", 
+    //   showMethod: "slideDown", 
+    //   hideMethod: "slideUp", 
+    //   oneSwitch : false
+    // });
     
   });
   
-  $("#demo2 div.expand").toggler({initShow: "div.collapse:eq(0)"});
+  // $("#demo2 div.expand").toggler({initShow: "div.collapse:eq(0)"});
  
-  $("#demo2").expandAll({
-    trigger: "div.expand", 
-    ref: "div.expand", 
-    showMethod: "slideDown", 
-    hideMethod: "slideUp", 
-    oneSwitch : false
-  });
+  // $("#demo2").expandAll({
+  //   trigger: "div.expand", 
+  //   ref: "div.expand", 
+  //   showMethod: "slideDown", 
+  //   hideMethod: "slideUp", 
+  //   oneSwitch : false
+  // });
 });

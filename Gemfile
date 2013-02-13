a3c102de76e0a5af328368d796a085e69f85cb80
Arindam Chakraborty <arismart99@gmail.com>
2013-02-13 23:03:56 +0530

    minor fixes in stories markup

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source :rubygems
-source 'http://rubygems.org'
 
 gem 'rails', '3.2.6'
 gem 'rake'
 gem "cached_model", '1.3.1'
-gem "json", '1.7.5'
+gem "json", '1.7.7'
 gem "capistrano", '2.13.4'
 gem "unicorn", '4.3.1'
 gem "mysql", '2.8.1'
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,9 @@ module Aapaurmain
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
+    
+    ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML) 
+    ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::JSON) 
 
     # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer

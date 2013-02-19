5fa066c93aa3601ad6b653f6ddf560cbeaa1db5e
Arindam Chakraborty <arismart99@gmail.com>
2013-02-19 21:03:17 +0530

    fixing backend for partial sends

--- app/models/panel.rb
+++ app/models/panel.rb
@@ -12,10 +12,10 @@ class Panel < ActiveRecord::Base
   def self.get_common_and_other_panels_for(his_id, my_id)
     common_panels = remaining_panels = []
     $r.multi do
-      common_panels = $r.sinter("user:#{his_id}:panels", "user:#{my_id}:panels").value
-      remaining_panels = $r.sdiff("user:#{his_id}:panels", "user:#{my_id}:panels").value
+      common_panels = $r.sinter("user:#{his_id}:panels", "user:#{my_id}:panels")
+      remaining_panels = $r.sdiff("user:#{his_id}:panels", "user:#{my_id}:panels")
     end
-    [common_panels, remaining_panels]
+    [common_panels.value, remaining_panels.value]
   end
 
 end

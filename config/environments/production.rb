d70d5f2ded73390dc2d306fe9e44114e04370313
Arindam Chakraborty <arismart99@gmail.com>
2013-03-31 18:17:10 +0530

    try adding active admin files to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Aapaurmain::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += [ '*.js', '*.css' ]
+  config.assets.precompile += [ 'active_admin.css', 'active_admin/print.css', 'active_admin.js', '*.js', '*.css' ]
   # %w( application-search.js application-all.js application-conversation.js application-user.js application-all.css application-search.css application-user.css )
 
   # Disable delivery errors, bad email addresses will be ignored
